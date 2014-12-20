4f89e76a86ae44cdeb357971e7a7659847142f74
voidmainvoid <ricardotubs79@gmail.com>
2014-12-20 09:34:52 +0100

    x

--- GitTest/src/com/hcs/demo/Main.java
+++ GitTest/src/com/hcs/demo/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Évike");
-		System.out.println("Hello Évike");
+		System.out.println("Hello Évi");
    
 	}
 
